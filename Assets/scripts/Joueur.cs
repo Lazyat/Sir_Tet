a434147b1ad5d6c3811eeeba35d5b7635ab99d6f
rhebok-git <geneenkei@gmail.com>
2018-02-28 10:07:37 +0100

    Mouvement

---
+++ Assets/scripts/Joueur.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Joueur : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	public int velociteSaut;
+	public int vitesseMouvement;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		velociteSaut = 5;
+		vitesseMouvement = 3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			rb.velocity = new Vector2(0, velociteSaut);
+			print("espace");
+		}
+
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			transform.Translate(Vector2.left * vitesseMouvement * Time.deltaTime);
+			print("gauche");
+		}
+
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			transform.Translate(Vector2.right * vitesseMouvement * Time.deltaTime);
+			print("droite");
+		}
+	}
+}
